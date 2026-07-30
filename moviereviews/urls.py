73dd949f7f0bd69e2d8f58bebc36a3a79ad0848d
EmmanuelHernandezp1 <311094449+EmmanuelHernandezp1@users.noreply.github.com>
2026-07-30 10:50:52 -0500

    primer commit

---
+++ moviereviews/urls.py
@@ -0,0 +1,17 @@
+"""
+URL configuration for moviereviews project.
+"""
+
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from movie import views as movieViews
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", movieViews.home, name="home"),
+    path("about/", movieViews.about, name="about"),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
